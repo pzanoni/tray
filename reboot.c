ad3637061687190b87d2f66fd0ffc0c08a135a4e
Claudio Matsuoka <cmatsuoka@gmail.com>
2008-05-28 18:05:59 -0300

    Fix icon path
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- reboot.c
+++ reboot.c
@@ -3,8 +3,8 @@
 #include <sys/types.h>
 #include <sys/signal.h>
 
-//#define ICON_PATH "/usr/share/icons/tray_reboot/"
-#define ICON_PATH "icons/"
+#define ICON_PATH "/usr/share/icons/tray_reboot/"
+//#define ICON_PATH "icons/"
 
 GtkWidget *window;
 GtkWidget *button1;
