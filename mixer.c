ff316681ba5c0704bf281df69d7783f17ac71e34
Claudio Matsuoka <cmatsuoka@gmail.com>
2008-09-18 12:14:28 -0300

    Volume setting works
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- mixer.c
+++ mixer.c
@@ -3,7 +3,9 @@
 #include <gdk/gdkx.h>
 #include <gdk/gdkkeysyms.h>
 #include <X11/Xlib.h>
-#include <getopt.h>
+#include <alsa/asoundlib.h>
+#include <alsa/mixer.h>
+
 
 #include "tray.h"
 
@@ -14,34 +16,90 @@ struct channel {
 	GtkWidget *vscale;
 	guint vscale_handler;
 	GtkWidget *mute;
+	long min, max;
 };
 
 GtkWidget *window;
 GtkWidget *hbox;
 GtkWidget *menu, *item;
 struct channel ch[1];
+snd_mixer_t *mixer;
+snd_mixer_elem_t *elem;
+snd_mixer_selem_id_t *sid;
 
 	 
 
-static int mixer_read(struct channel *c)
+static int mixer_init()
 {
+	snd_mixer_elem_t *e;
+
+	snd_mixer_selem_id_alloca(&sid);
+	if (snd_mixer_open(&mixer, 0) < 0) {
+		fprintf(stderr, "can't open mixer\n");
+		exit(1);
+	}
+	snd_mixer_attach(mixer, "default");
+	snd_mixer_selem_register(mixer, NULL, NULL);
+	snd_mixer_load(mixer);
+	
+	snd_mixer_selem_id_set_name(sid, "PCM");
+	elem = snd_mixer_find_selem(mixer, sid);
+	if (!elem) {
+		fprintf(stderr, "can't find PCM element\n");
+		exit(1);
+	}
+
+	snd_mixer_selem_get_playback_volume_range(elem, &ch[0].min, &ch[0].max);
+
 	return 0;
 }
 
-
-static int vol_change(struct channel *c)
+static int mixer_get(struct channel *c)
 {
-	return 0;
+	long l = snd_mixer_selem_get_playback_volume(elem,
+				SND_MIXER_SCHN_FRONT_LEFT, &l);
+	long r = snd_mixer_selem_get_playback_volume(elem,
+				SND_MIXER_SCHN_FRONT_RIGHT, &r);
+
+	printf("%ld, %ld\n", l, r);
+	return (l + r) / 2;
 }
 
-static int scale_scroll(struct channel *c)
+static void mixer_set(struct channel *c, int vol)
 {
+	snd_mixer_selem_set_playback_volume(elem, SND_MIXER_SCHN_FRONT_LEFT, vol);
+	snd_mixer_selem_set_playback_volume(elem, SND_MIXER_SCHN_FRONT_RIGHT, vol);
+mixer_get(c);
+}
+
+static void vol_change(GtkRange *range, struct channel *c)
+{
+	int vol = c->max * gtk_range_get_value(range) / 100;
+	mixer_set(c, vol);
+}
+
+static int scale_scroll(GtkScale* scale, GdkEventScroll *e, struct channel *c)
+{
+	int val = gtk_range_get_value((GtkRange*)scale);
+
+	val += e->direction == GDK_SCROLL_UP ? 2 : -2;
+	gtk_range_set_value((GtkRange*)scale, CLAMP(val, 0, 100));
+
 	return 0;
 }
 
 static void click()
 {
-	gtk_widget_show_all(window);
+	static int show = 0;
+
+	if (show) {
+		gtk_widget_hide_all(window);
+	} else {
+		gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_MOUSE);
+		gtk_widget_show_all(window);
+	}
+
+	show ^= 1;
 }
 
 static void popup()
@@ -63,6 +121,9 @@ int main(int argc, char **argv)
 	textdomain("tray_mixer");
 
 	gtk_init(&argc, &argv);
+
+	mixer_init();
+
 	icon = (struct GtkStatusIcon *)
 			gtk_status_icon_new_from_file(ICON_PATH "speaker.png");
 	g_signal_connect(G_OBJECT(icon), "activate", G_CALLBACK(click), NULL);
@@ -76,14 +137,24 @@ int main(int argc, char **argv)
 	g_signal_connect(G_OBJECT(item), "activate", G_CALLBACK(quit), NULL);
 
 	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	gtk_container_set_border_width(GTK_CONTAINER(window), 30);
+	gtk_window_set_decorated(GTK_WINDOW(window), FALSE);
+	gtk_container_set_border_width(GTK_CONTAINER(window), 10);
+	gtk_window_set_skip_taskbar_hint(GTK_WINDOW(window), TRUE);
+	gtk_window_set_skip_pager_hint(GTK_WINDOW(window), TRUE);
+	gtk_window_set_type_hint(GTK_WINDOW(window),
+					GDK_WINDOW_TYPE_HINT_DIALOG);
+	gtk_window_set_default_size(GTK_WINDOW(window), 60, 140);
+
 	hbox = gtk_hbox_new(TRUE, TRUE);
+	gtk_container_add(GTK_CONTAINER(window), hbox);
 
 	ch[0].vbox = gtk_vbox_new(TRUE, TRUE);
 	ch[0].vscale = gtk_vscale_new(GTK_ADJUSTMENT(
-		gtk_adjustment_new(mixer_read(&ch[0]), 0, 100, 0, 0, 0)));
+		gtk_adjustment_new(mixer_get(&ch[0]), 0, 100, 0, 0, 0)));
 	gtk_scale_set_draw_value(GTK_SCALE(ch[0].vscale), FALSE);
 	gtk_range_set_inverted(GTK_RANGE(ch[0].vscale), TRUE);
+	gtk_container_add(GTK_CONTAINER(hbox), ch[0].vbox);
+	gtk_container_add(GTK_CONTAINER(ch[0].vbox), ch[0].vscale);
 
 	ch[0].vscale_handler = g_signal_connect((gpointer)ch[0].vscale,
 			"value_changed", G_CALLBACK(vol_change), &ch[0]);
